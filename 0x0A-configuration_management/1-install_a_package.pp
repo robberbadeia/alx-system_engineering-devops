b13ba4838e8e99c21c110d54dc447aa781c564f0
RobberBadeia <robber.badeia@gmail.com>
2024-01-19 16:02:48 +0200

    .

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,4 @@
 # Using Puppet, install flask from pip3.
 package { 'flask':
-    command => '/usr/bin/pip3 install Flask',
-    ensure  => '2.1.0',
+    command => '/usr/bin/pip3 install Flask==2.1.0',
 }
