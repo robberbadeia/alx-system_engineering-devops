823b97bf0319b9da7841e64c5d9b01ae1f9af2a4
robberbadeia <robber.badeia@gmail.com>
2024-03-06 10:24:21 +0200

    Debg #3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+#Solve internal server error
+
+exec { 'fix typo':
+  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
