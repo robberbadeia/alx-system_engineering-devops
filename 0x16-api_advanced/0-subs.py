88c245607dba5814fb7a1c1d468deae80895c80c
robberbadeia <robber.badeia@gmail.com>
2024-03-06 15:48:18 +0200

    T00

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,21 +3,33 @@
 T00
 """
 import requests
+from requests import *
 
 
 def number_of_subscribers(subreddit):
     """
     Function Implementation
     """
-    user_agent = 'Mozilla/5.0'
+    client_id = 'xnDxd-7qr-aFwHQ7_k-kKw'
+    client_secret = '4KVZ5lw12yu3KYQZ8ytXZb0ejaShQQ'
+    user_agent = 'Chrome/122.0.6261.95'
 
-    headers = {
-        'User-Agent': user_agent
-    }
+    auth = requests.auth.HTTPBasicAuth(client_id, client_secret)
+    headers = {'User-Agent': user_agent}
+    data = {'grant_type': 'password', 'username': 'RobbairBadeia', 'password': 'Godislove'}
+    auth_url = 'https://www.reddit.com/api/v1/access_token'
+
+
+    # Get OAuth token
+    token_response = requests.post(auth_url, auth=auth, data=data, headers=headers)
+    token = token_response.json().get('access_token')
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'Authorization': f'Bearer {token}', 'User-Agent': user_agent}
+
     res = requests.get(url, headers=headers, allow_redirects=False)
     if res.status_code != 200:
+        print(res.status_code)
         return 0
     results = res.json().get('data')
     return results.get('subscribers')
